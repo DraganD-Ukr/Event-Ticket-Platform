47f5836088a15c2e0286fc9cebdf725c8e2f6aa6
DraganD <dmitrijrolkin@gmail.com>
2025-07-13 11:08:41 +0100

    Initial spring boot project set up

---
+++ Event_Ticket_Platform_API/src/main/java/com/dragand/event_ticket_platform_api/EventTicketPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.dragand.event_ticket_platform_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketPlatformApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventTicketPlatformApiApplication.class, args);
+    }
+
+}
---
+++ Event_Ticket_Platform_API/src/test/java/com/dragand/event_ticket_platform_api/EventTicketPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragand.event_ticket_platform_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
